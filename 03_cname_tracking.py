cb2f995fb9776ad9030090c72a0956ff85206bb6
Habuchi_Kazushi <k.habuchi@kiyono-co.jp>
2021-06-29 17:52:10 +0900

    fix_1

---
+++ 03_cname_tracking.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, make_response, send_file
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    host = request.headers['HOST']
+    if host == 'car.com:5000':
+        return make_response(render_template("03_car_com_cname.html"))
+    elif host == 'news.com:5000':
+        return make_response(render_template("03_news_com_cname.html"))
+    elif host == 'banner.com:5000' or host == 'banner.car.com:5000' or host == 'banner.news.com:5000':
+        referer_history = request.cookies.get('referer_history')
+        if referer_history == 'https://car.com:5000/':
+            response = send_file('banners/banner_car.jpeg', mimetype='image/jpg')
+        else:
+            response = send_file('banners/banner_random.png', mimetype='image/png')
+            response.set_cookie('referer_history', request.referrer, samesite='None', secure=True)
+        return response
+    else:
+        return render_template('404.html'), 404
+
+
+if __name__ == "__main__":
+    app.run(ssl_context=('ssl/server.crt', 'ssl/server.key'), host='0.0.0.0', debug=True)
--- test_anagram.py
+++ test_anagram.py
@@ -35,3 +35,4 @@ class TestIsAnagram(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+    
